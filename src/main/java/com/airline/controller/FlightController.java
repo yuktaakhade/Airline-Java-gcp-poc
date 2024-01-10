58609b5c5240cfc478740d3adad3bdba2d0c3ffd
Ashiph <ashiphs@cybage.com>
2024-01-10 14:49:38 +0530

    cors updated

--- src/main/java/com/airline/controller/FlightController.java
+++ src/main/java/com/airline/controller/FlightController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/flight")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class FlightController {
 
     @Autowired
